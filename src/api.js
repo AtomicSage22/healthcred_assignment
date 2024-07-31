0edd2ccc5504972931bf67ab13a8c98c7b6e0f81
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2024-07-31 18:03:42 +0530

    Update env variables for prod build

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+
 export const fetchData = async () => {
+     const url = import.meta.env.VITE_API_URL;
   try {
-    const response = await axios.get('/api');
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
