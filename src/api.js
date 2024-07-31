2c9b47432cb9b21dc2f290aeee3d79629e873dd7
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2024-07-31 15:31:25 +0530

    chore: Update API endpoint in fetchData function

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get('https://15734573-beec-42a6-9f83-e25fb78af6f2.mock.pstmn.io/hcassigment');
+    const response = await axios.get('/api');
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
